27698a3457e42cb794612fea92119a39f3ff146f
xxfast <isuru@xfastgames.com>
2020-05-23 17:27:30 +1000

    Fix oak leave runners to randomly grow like vines

--- src/main/kotlin/com/xfastgames/witness/blocks/leaves/OakLeavesRunners.kt
+++ src/main/kotlin/com/xfastgames/witness/blocks/leaves/OakLeavesRunners.kt
@@ -2,6 +2,7 @@ package com.xfastgames.witness.blocks.leaves
 
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry
+import net.minecraft.block.BlockState
 import net.minecraft.block.Material
 import net.minecraft.block.MaterialColor
 import net.minecraft.block.VineBlock
@@ -10,7 +11,9 @@ import net.minecraft.client.color.item.ItemColorProvider
 
 class OakLeavesRunners : VineBlock(FabricBlockSettings.of(Material.PLANT).nonOpaque().materialColor(MaterialColor.FOLIAGE)) {
     init {
-        ColorProviderRegistry.BLOCK.register(BlockColorProvider { state, view, pos, tintIndex -> 0xA0AB42 }, this)
-        ColorProviderRegistry.ITEM.register(ItemColorProvider { stack, tintIndex -> 0xA0AB42 }, this)
+        ColorProviderRegistry.BLOCK.register(BlockColorProvider { _, _, _, _ -> 0xA0AB42 }, this)
+        ColorProviderRegistry.ITEM.register(ItemColorProvider { _, _ -> 0xA0AB42 }, this)
     }
+
+    override fun hasRandomTicks(state: BlockState?): Boolean = true
 }
