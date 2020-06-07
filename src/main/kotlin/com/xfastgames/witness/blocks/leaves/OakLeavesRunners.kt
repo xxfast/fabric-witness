b7bf837a98a4feec34989faab4306d578c0602bb
xxfast <isuru@xfastgames.com>
2020-06-07 18:41:31 +1000

    Fix oak leave runners

--- src/main/kotlin/com/xfastgames/witness/blocks/leaves/OakLeavesRunners.kt
+++ src/main/kotlin/com/xfastgames/witness/blocks/leaves/OakLeavesRunners.kt
@@ -2,14 +2,20 @@ package com.xfastgames.witness.blocks.leaves
 
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry
-import net.minecraft.block.BlockState
 import net.minecraft.block.Material
-import net.minecraft.block.MaterialColor
 import net.minecraft.block.VineBlock
 import net.minecraft.client.color.block.BlockColorProvider
 import net.minecraft.client.color.item.ItemColorProvider
+import net.minecraft.sound.BlockSoundGroup
 
-class OakLeavesRunners : VineBlock(FabricBlockSettings.of(Material.PLANT).nonOpaque().materialColor(MaterialColor.FOLIAGE)) {
+class OakLeavesRunners : VineBlock(
+    FabricBlockSettings
+        .of(Material.REPLACEABLE_PLANT)
+        .noCollision()
+        .ticksRandomly()
+        .strength(0.2f)
+        .sounds(BlockSoundGroup.GRASS)
+) {
 
     companion object {
         val BLOCK by lazy { OakLeavesRunners() }
@@ -19,6 +25,4 @@ class OakLeavesRunners : VineBlock(FabricBlockSettings.of(Material.PLANT).nonOpa
         ColorProviderRegistry.BLOCK.register(BlockColorProvider { _, _, _, _ -> 0xA0AB42 }, this)
         ColorProviderRegistry.ITEM.register(ItemColorProvider { _, _ -> 0xA0AB42 }, this)
     }
-
-    override fun hasRandomTicks(state: BlockState?): Boolean = true
 }
