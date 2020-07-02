875eb93b4bc5805342a19eb9e13201edbe3f432f
xxfast <isuru@xfastgames.com>
2020-07-02 15:26:19 +1000

    Add mixins to support non-mob entity rendering

--- src/main/kotlin/com/xfastgames/witness/entities/PuzzleFrameEntity.kt
+++ src/main/kotlin/com/xfastgames/witness/entities/PuzzleFrameEntity.kt
@@ -4,20 +4,21 @@ import com.xfastgames.witness.Witness
 import com.xfastgames.witness.entities.renderers.PuzzleFrameRenderer
 import com.xfastgames.witness.utils.Clientside
 import com.xfastgames.witness.utils.registerEntity
-import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricDefaultAttributeRegistry
 import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricEntityTypeBuilder
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry
 import net.minecraft.client.render.entity.EntityRenderDispatcher
+import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityDimensions
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.SpawnGroup
-import net.minecraft.entity.mob.MobEntity
-import net.minecraft.entity.mob.MobEntityWithAi
+import net.minecraft.entity.decoration.AbstractDecorationEntity
+import net.minecraft.network.Packet
+import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket
+import net.minecraft.sound.SoundEvents
 import net.minecraft.util.Identifier
 import net.minecraft.world.World
 
-class PuzzleFrameEntity(entityType: EntityType<out MobEntityWithAi?>?, world: World?) :
-    MobEntityWithAi(entityType, world) {
+class PuzzleFrameEntity(world: World) : AbstractDecorationEntity(ENTITY_TYPE, world) {
 
     companion object : Clientside {
         val IDENTIFIER = Identifier(Witness.IDENTIFIER, "puzzle_frame")
@@ -25,7 +26,7 @@ class PuzzleFrameEntity(entityType: EntityType<out MobEntityWithAi?>?, world: Wo
             registerEntity(IDENTIFIER) {
                 FabricEntityTypeBuilder
                     .create(SpawnGroup.CREATURE) { entityType: EntityType<PuzzleFrameEntity>, world: World ->
-                        PuzzleFrameEntity(entityType, world)
+                        PuzzleFrameEntity(world)
                     }
                     .dimensions(EntityDimensions.fixed(0.75f, 0.75f))
                     .build()
@@ -33,16 +34,22 @@ class PuzzleFrameEntity(entityType: EntityType<out MobEntityWithAi?>?, world: Wo
 
         val ENTITY: Companion = this
 
-        init {
-            FabricDefaultAttributeRegistry.register(
-                ENTITY_TYPE, MobEntity.createMobAttributes()
-            )
-        }
-
         override fun onClient() {
             EntityRendererRegistry.INSTANCE.register(ENTITY_TYPE) { dispatcher: EntityRenderDispatcher?, context: EntityRendererRegistry.Context? ->
                 PuzzleFrameRenderer(dispatcher)
             }
         }
+    }
+
+    override fun createSpawnPacket(): Packet<*> = EntitySpawnS2CPacket(this, type, facing.id, this.decorationBlockPos)
+
+    override fun onPlace() {
+        playSound(SoundEvents.ENTITY_DOLPHIN_PLAY, 1.0f, 1.0f)
+    }
+
+    override fun getHeightPixels(): Int = 12
+    override fun getWidthPixels(): Int = 12
+
+    override fun onBreak(entity: Entity?) {
     }
 }
--- src/main/kotlin/com/xfastgames/witness/entities/renderers/PuzzleFrameRenderer.kt
+++ src/main/kotlin/com/xfastgames/witness/entities/renderers/PuzzleFrameRenderer.kt
@@ -2,18 +2,72 @@ package com.xfastgames.witness.entities.renderers
 
 import com.xfastgames.witness.Witness
 import com.xfastgames.witness.entities.PuzzleFrameEntity
-import com.xfastgames.witness.entities.models.PuzzleFrameModel
+import net.minecraft.block.BlockState
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.render.OverlayTexture
+import net.minecraft.client.render.TexturedRenderLayers
+import net.minecraft.client.render.VertexConsumerProvider
 import net.minecraft.client.render.entity.EntityRenderDispatcher
-import net.minecraft.client.render.entity.MobEntityRenderer
+import net.minecraft.client.render.entity.EntityRenderer
+import net.minecraft.client.util.ModelIdentifier
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.client.util.math.Vector3f
 import net.minecraft.util.Identifier
 
 /*
 * A renderer is used to provide an entity model, shadow size, and texture.
 */
 class PuzzleFrameRenderer(entityRenderDispatcher: EntityRenderDispatcher?) :
-    MobEntityRenderer<PuzzleFrameEntity, PuzzleFrameModel>(entityRenderDispatcher, PuzzleFrameModel(), 0.5f) {
+    EntityRenderer<PuzzleFrameEntity>(entityRenderDispatcher) {
+
+    private val FRAME = ModelIdentifier("grass")
+    private val client: MinecraftClient = MinecraftClient.getInstance()
 
     override fun getTexture(entity: PuzzleFrameEntity): Identifier {
         return Identifier(Witness.IDENTIFIER, "textures/entity/cube.png")
+    }
+
+    override fun render(
+        entity: PuzzleFrameEntity,
+        yaw: Float,
+        tickDelta: Float,
+        matrices: MatrixStack,
+        vertexConsumers: VertexConsumerProvider,
+        light: Int
+    ) {
+        super.render(entity, yaw, tickDelta, matrices, vertexConsumers, light)
+        matrices.push()
+        val direction = entity.horizontalFacing
+        val vec3d = getPositionOffset(entity, tickDelta)
+        matrices.translate(-vec3d.getX(), -vec3d.getY(), -vec3d.getZ())
+        val directionalOffset = 0.46875
+        matrices.translate(
+            direction.offsetX.toDouble() * directionalOffset,
+            direction.offsetY.toDouble() * directionalOffset,
+            direction.offsetZ.toDouble() * directionalOffset
+        )
+        matrices.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(entity.pitch))
+        matrices.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(180.0f - entity.yaw))
+        val bl = entity.isInvisible
+        if (!bl) {
+            val blockRenderManager = client.blockRenderManager
+            val bakedModelManager = blockRenderManager.models.modelManager
+            val modelIdentifier = FRAME
+            matrices.push()
+            matrices.translate(-0.5, -0.5, -0.5)
+            blockRenderManager.modelRenderer.render(
+                matrices.peek(),
+                vertexConsumers.getBuffer(TexturedRenderLayers.getEntitySolid()),
+                null as BlockState?,
+                bakedModelManager.getModel(modelIdentifier),
+                1.0f,
+                1.0f,
+                1.0f,
+                light,
+                OverlayTexture.DEFAULT_UV
+            )
+            matrices.pop()
+        }
+        matrices.pop()
     }
 }
